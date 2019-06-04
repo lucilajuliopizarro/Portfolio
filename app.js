120eace8e0ac635908cf4e1d47d5bb819b19059f
lucilajuliopizarro <lucitajulio@hotmail.com>
2019-06-04 18:13:51 -0400

    avanze portafolio

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById('Sobreyo').addEventListener('click', evento => {
+  document.getElementById('inicio').style.display = 'none';
+  document.getElementById('yo').style.display = 'block';
+});
