88a14d552e19e2a16d4d552625c11d71de266c09
lucilajuliopizarro <lucitajulio@hotmail.com>
2019-06-06 08:28:37 -0400

    avanze portafolio tercera parte

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ document.getElementById('Sobreyo').addEventListener('click', evento => {
   document.getElementById('inicio').style.display = 'none';
   document.getElementById('yo').style.display = 'block';
 });
+
+document.getElementById('Htecnicas').addEventListener('click', evento => {
+  document.getElementById('yo').style.display = 'none';
+  document.getElementById('Habilidades Tecnicas').style.display = 'block';
+});
+
+document.getElementById('experiencias').addEventListener('click', evento => {
+  document.getElementById('Habilidades Tecnicas').style.display = 'none';
+  document.getElementById('mis Experiencias').style.display = 'block';
+});
